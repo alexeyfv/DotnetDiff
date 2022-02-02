957c843d48abd7bc7e013e9a5a8b803d7e76ddda
Alexey Fedorov <alexey.fedorov.dev@gmail.com>
2022-02-03 00:03:26 +0300

    Add Class2.cs to TestProject1

---
+++ DotnetDiff.TestProject1/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetDiff.TestProject1
+{
+    internal class Class2
+    {
+        public int MyProperty1 { get; set; }
+    }
+}
