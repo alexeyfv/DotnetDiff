1eb6b0b07ed9742ee7f493d2253225e337e156ff
Alexey Fedorov <alexey.fedorov.dev@gmail.com>
2022-02-03 00:09:16 +0300

    Add Class3.cs to TestProject3

---
+++ DotnetDiff.TestProject3/Class3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetDiff.TestProject3
+{
+    internal class Class3
+    {
+    }
+}
