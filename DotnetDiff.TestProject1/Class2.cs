63035a6538821277cab1fb5143370e7e8430983d
Alexey Fedorov <alexey.fedorov.dev@gmail.com>
2022-02-03 00:04:04 +0300

    Change Class2.cs in TestProject1

--- DotnetDiff.TestProject1/Class2.cs
+++ DotnetDiff.TestProject1/Class2.cs
@@ -11,5 +11,7 @@ namespace DotnetDiff.TestProject1
         public int MyProperty1 { get; set; }
 
         public int MyProperty2 { get; set; }
+
+        public int MyProperty3 { get; set; }
     }
 }
